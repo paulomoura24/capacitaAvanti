bf99f664b6cfc675af1ce7b2a3d9ac83cbb8fefb
paulomoura24 <paulomouramoura1412@gmail.com>
2025-06-06 19:09:20 +0000

    Criação e Utilização de Componentes

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from './components/Button';
+
+const App = () => {
+  return (
+    <div>
+      <h2> Aula sobre componentes </h2>
+      
+      <Button label="Clique aqui" />
+      <Button label="Enviar" />
+    </div>
+  );
+};
+
+export default App;
