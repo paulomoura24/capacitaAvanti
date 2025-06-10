d6ea9f41b03eb9009209229f6e1a82413a6b4bec
paulomoura24 <paulomouramoura1412@gmail.com>
2025-06-10 10:58:31 +0000

    Implementação do Hook useState

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Button from './components/Button';
+import Contador from './components/Contador';
 
 const App = () => {
   return (
     <div>
       <h2> Aula sobre componentes </h2>
-      
       <Button label="Clique aqui" />
       <Button label="Enviar" />
+
+      <div>
+        <Contador></Contador>
+      </div>
     </div>
+
   );
 };
 
---
+++ meu-projeto/src/components/Contador.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+
+const Contador = () => {
+    const [contador, setContador] = useState[0];
+
+    const incrementar = () => {
+        setContador(contador + 1);
+    };
+
+    return (
+        <div>
+            <p>Contador: {contador}</p>
+            <button onClick={incrementar}>Incrementar</button>
+        </div>
+    );
+};
+
+export default Contador;
